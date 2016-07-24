75d89440c7144af3c99f559165b7bda1748f490e
Sethu Iyer <sethuiyer95@gmail.com>
2016-07-24 16:55:29 +0530

    Finished Problem set 2

---
+++ Data Wrangling with MongoDB/data-in-more-complex-formats/flights.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Complete the 'extract_airports' function so that it returns a list of airport
+codes, excluding any combinations like "All".
+"""
+
+from bs4 import BeautifulSoup
+html_page = "options.html"
+
+
+def extract_airports(page):
+    data = []
+    with open(page, "r") as html:
+        # do something here to find the necessary values
+        soup = BeautifulSoup(html, "lxml")
+        options = soup.find_all('option')
+        for o in options:
+            if len(o['value']) == 3 and o['value'] != "All":
+                data.append(o['value'])
+    return data
+
+
+def test():
+    data = extract_airports(html_page)
+    assert len(data) == 15
+    assert "ATL" in data
+    assert "ABR" in data
+
+test()
